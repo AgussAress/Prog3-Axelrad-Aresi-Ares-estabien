50d1980d1d17e6a3154e7089353b8044051b807c
MaximaAresi <maresilavalle@udesa.edu.ar>
2024-09-23 11:34:07 -0300

    mas cambios a Favoritos

--- proyecto-integrador/src/screens/Favoritos.js
+++ proyecto-integrador/src/screens/Favoritos.js
@@ -5,32 +5,38 @@ class Favoritos extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            pelicula:[]
+            peliculasFav:[]
+
         };
-    }
-    apiCall(url) {
-        fetch(url)
-            .then(response => response.json())
-            .then((data) => {
-                this.setState({
-                    pelicula: data
-                });
-            })
-            .catch(error => console.log(error));
     }
 
     componentDidMount(){
-        console.log('props detalle', this.props)
-        const movie_id = this.props.match.params.id
-        this.apiCall(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${APIKEY}`)
+        let storage = localStorage.getItem("Favs") //recupero el storage donde estan los ids
+        if (storage!==null){
+            let storageParseado = JSON.parse(storage) //si no es null, y hay algo, lo parseo y busco con fetch el detalle
+
+            Promise.all( //el fetch resulve info, el then la desenvuelve y el promise all junta todo, por ende puedo hacer thens de todo junto y recibir la data
+                storageParseado.map(elm =>
+                    fetch(`https://api.themoviedb.org/3/movie/${elm}?api_key=${APIKEY}`)
+                    .then(resp => resp.json())
+                )
+            )
+           // .then(data=> console.log('Then del promiseAll', data)) // con este camino obtengo un array completo con todo agrupado
+            .then(data => this.setState({peliculasFav:data}))
+        }
     }
 
     render(){
-        return ( 
-            <React.Fragment>
-                    <h1 className = "home__section-h1">Estos son los Favoritos</h1>
-                    <p className = "home__div-p">Tus peliculas favoritas son: {this.state.pelicula.id}</p>
-            </React.Fragment>
+        return ( // probar hacer un if o llamar a un componente como cartelera de donde yo pueda sacar la imagen de la pelicula en favoritos y el boton de descripcion Y LINKEAR AL DETALLE 
+            <div>
+                    <h1 className = "home__section-h1">Estos son los Favoritos</h1> 
+                    {this.state.peliculasFav.length >0 ? 
+                    this.state.peliculasFav.map(elm => 
+                    <h1>{elm.title}</h1>)
+                    :
+                    <h1>No hay peliculas Favoritas</h1>
+                    } 
+            </div>
     )}
     }
 
