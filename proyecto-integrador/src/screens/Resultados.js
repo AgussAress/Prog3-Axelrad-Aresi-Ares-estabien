79254dddd1ff331f0901410dfe1e167edd8894b9
AgussAress <aares@udesa.edu.ar>
2024-09-25 19:06:46 -0300

    loader en resultados y un poco de estilos

--- proyecto-integrador/src/screens/Resultados.js
+++ proyecto-integrador/src/screens/Resultados.js
@@ -5,38 +5,64 @@ export default class Resultados extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            resultados: []
+            resultados: [],
+            loading: true
         }
     }
 
     componentDidMount() {
         const loQueBuscaElUsuario = this.props.history.location.state.busqueda
+        const minimumLoadingTime = new Promise(resolve => {
+            setTimeout(resolve, 2000)
+        })
 
-        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${loQueBuscaElUsuario}`)
+        const apiCall = fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${loQueBuscaElUsuario}`)
             .then(response => response.json())
-            .then(data => {
-                this.setState({ resultados: data.results })
+            Promise.all([minimumLoadingTime, apiCall])
+            .then(([_, data]) => {
+                this.setState({ 
+                    resultados: data.results || [],
+                    loading: false
+                });
             })
-            .catch(error => console.log(error))
+            .catch(error => {
+                console.log(error)
+                this.setState({ loading: false })
+            });
     }
 
     render() {
         return (
-            <div>
-                {this.state.resultados.length > 0 ? (
-                    this.state.resultados.map((pelicula, index) => (
-                        <div key={index}>
-                            <img 
-                                src={`https://image.tmdb.org/t/p/w342/${pelicula.poster_path}`} 
-                                alt={pelicula.title} 
-                            />
-                            <p>{pelicula.title}</p>
-                        </div>
-                    ))
+            <div className="peliculas__section">
+                <h1 className="res-bus">Resultados de búsqueda</h1>
+                {this.state.loading ? (   
+                    <div className="cargando__gif-container">
+                        <img src="/imgs/carga.webp" alt="Cargando..." className="cargando__gif" />
+                    </div>
                 ) : (
-                    <h1>No hay resultados de búsqueda</h1>
+                    this.state.resultados.length > 0 ? (
+                        <div className="peliculas__section-div">
+                            {this.state.resultados.map((pelicula, index) => (
+                                <article className="peliculas__div-article" key={index}>
+                                    <img className='peliculas__div-img'
+                                        src={`https://image.tmdb.org/t/p/w342/${pelicula.poster_path}`} 
+                                        alt={pelicula.title} 
+                                    />
+                                    <h2 className='peliculas__div-h2'>{pelicula.title}</h2>
+                                </article>
+                            ))}
+                        </div>
+                    ) : (
+                        <div>
+                            <br></br>
+                            <hr></hr>
+                        <h1 className="res-bus" >No hay resultados de búsqueda</h1>
+                        </div>
+                    )
                 )}
             </div>
+
+
         );
     }
 }
